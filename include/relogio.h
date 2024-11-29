298c810642a60496d8c59abaf7659d6886d71bbf
Geovana Espinosa <geovanaribeiroaraujoespinosa@gmail.com>
2024-11-29 12:09:20 -0300

    Update relogio.h

--- include/relogio.h
+++ include/relogio.h
@@ -1,3 +1,23 @@
 #pragma once
 
+#define RELOGIO_H
+
+#include <stdio.h>
+
+// horários do dia
+typedef enum {
+    CAFE_DA_MANHA,
+    ALMOCO,
+    JANTA
+} Horarios;
+
+// variável global para o relógio
 extern int hora_atual;
+
+// manipulação do relógio
+void relogio_set(Horarios horario);    
+void relogio_formatado(char *buffer);     
+void relogio_incrementar(int segundos);    
+int relogio_obter();                       
+
+
